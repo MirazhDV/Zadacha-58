6a89f279e73a0609327eecf742210697d2d4acc0
uas-92 <uas-92@mail.ru>
2023-01-13 13:01:22 +0700

    +

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 58: Задайте две матрицы. 
+// Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+int[,] InitMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+int lineMatrix = 2;
+int columnMatrix =2;
+void MultiplyMatrix(int[,] firstMatrix, int[,] secondMatrix, int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < firstMatrix.GetLength(1); k++)
+      {
+        sum += firstMatrix[i,k] * secondMatrix[k,j];
+      }
+      resultMatrix[i,j] = sum;
+    }
+  }
+}
+int[,] matrixFirst = InitMatrix(lineMatrix, columnMatrix);
+int[,] matrixSecond = InitMatrix(lineMatrix, columnMatrix);
+int[,] resultMatrix = InitMatrix(lineMatrix, columnMatrix);
+
+PrintMatrix(matrixFirst);
+Console.WriteLine("---");
+PrintMatrix(matrixSecond);
+Console.WriteLine("||");
+MultiplyMatrix(matrixFirst,matrixSecond,resultMatrix);
+PrintMatrix(resultMatrix);
